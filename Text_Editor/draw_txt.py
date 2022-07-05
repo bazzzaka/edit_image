b601083990bb610a561b2bcf50eefeda50b34a26
bazzzaka <bazaka.developer@gmail.com>
2022-07-05 13:26:34 +0300

    added processing function. Have issues

--- Text_Editor/draw_txt.py
+++ Text_Editor/draw_txt.py
@@ -1,30 +1,34 @@
 import requests
+from io import BytesIO
 from PIL import Image, ImageFont, ImageDraw
 
 
 class DrawTxt:
 
     @staticmethod
-    def draw_txt(image, position, text, font, align):
+    def draw_txt(image, position, text, size_txt, fill, font):
         print('work def draw_txt')
-        draw = ImageDraw.Draw(image)
-        get_font = ImageFont.truetype(font, position)
-        draw.text(position, text, get_font, align)
+        im = Image.open(BytesIO(image))
+        draw = ImageDraw.Draw(im, mode='RGBA')
+        get_font = ImageFont.truetype(font, size_txt)
+        draw.text(position, text, fill, get_font)
         print('return image with text')
         return draw
 
     @staticmethod
-    def image_text(image_path, image_url, position, text, font, align):
+    def image_text(image_path, image_url, position, text, size_txt, fill, font):
         """
 
-      :param image_path: the image that is on the computer
-      :param image_url: link to the image to be downloaded
-      :param position: text position
-      :param text: text to insert into the image
-      :param font: text font
-      :param align: edge alignment
-      :return: image with text
-      """
+        :param fill:
+        :param size_txt:
+        :param image_path: the image that is on the computer
+        :param image_url: link to the image to be downloaded
+        :param position: text position
+        :param text: text to insert into the image
+        :param font: text font
+        :param align: edge alignment
+        :return: image with text
+        """
         print('work def image_text')
 
         if image_path is None:
@@ -34,17 +38,19 @@ class DrawTxt:
             im = Image.open(r'{}'.format(image_path))
 
         print('image put in draw_txt')
-        result = DrawTxt.draw_txt(im, position, text, font, align)
+        result = DrawTxt.draw_txt(im, position, text, size_txt, fill, font)
         return result
 
 
 if __name__ == '__main__':
-    result = DrawTxt.image_text(
+    result_im = DrawTxt.image_text(
         image_path=None,
         image_url='https://www.imgonline.com.ua/examples/bee-on-daisy.jpg',
         position=(20, 20),
-        text='Хай живе надія',
+        text='some_txt',
+        size_txt=20,
+        fill='black',
         font='D:\Fonts SUA\Pangolin-Regular.ttf',
-        align='left'
     )
-    result.save()
+    result_im.save('test_image.png')
+
